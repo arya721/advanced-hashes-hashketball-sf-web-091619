a2a9433c44129e0df4ab52e2871de8638b42115b
Arya <arya721@gmail.com>
2019-09-14 19:24:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,5 @@
+=begin
+
 def game_hash
   {
     :away => {  :team_name => "Charlotte Hornets",
@@ -266,4 +268,28 @@ def long_name_steals_a_ton?
     end
   end
   return true if steals == player_with_longest_name
+end
+
+
+
+
+=end
+
+
+
+require "pry"
+
+
+def good_practices
+  game_hash.each do |location, team_data|
+    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+    binding.pry
+    team_data.each do |attribute, data|
+      #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+
+      #what is 'data' at each loop throughout .each block? when will the following line of code work and when will it break?
+      data.each do |data_item|
+      end
+    end
+  end
 end
